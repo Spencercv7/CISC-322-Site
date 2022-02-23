d172bb1f92c592b8142a0ec189fc7b6f7c59cb7c
Spencer Venable <46535891+Spencercv7@users.noreply.github.com>
2022-02-22 21:15:44 -0500

    Add: Assignemnt video link.

--- src/components/AssignmentCard.svelte
+++ src/components/AssignmentCard.svelte
@@ -5,8 +5,9 @@
 </script>
 
 <li>
-    <p>{data.title}</p>
-    <a href={`/pdf/${data.pdf}`}>View Document</a>
+    <p>{data.title ? data.title : "Coming Soon..."}</p>
+    <a href={`/pdf/${data.pdf}`} target="_blank">View Document</a>
+    <a href={`${data.videoURL}`} target="_blank">View Video</a>
 </li>
 
 <style>
@@ -18,10 +19,17 @@
         border-radius: 2%;
         position: relative;
         transition: all .1s;
+
+        display: grid;
+        grid-template-rows: repeat(2, 1fr);
     }
 
     li:hover {
         form: scale(1.01)
+    }
+
+    a {
+        place-self: center;
     }
 
     p {
