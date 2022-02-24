57e8254f6119c2aa33cf1def459b7e3a63beb463
Spencercv7 <spencercv7@gmail.com>
2022-02-24 14:08:29 -0500

    Update: modified assignment cards design.

--- src/components/AssignmentCard.svelte
+++ src/components/AssignmentCard.svelte
@@ -5,43 +5,86 @@
 </script>
 
 <li>
-    <p>{data.title ? data.title : "Coming Soon..."}</p>
-   
+    <span>{data.title ? data.title : "Coming Soon..."}</span>
     {#if data.title !== ""}
-        <a href={`/pdf/${data.pdf}`} target="_blank">View Document</a>
-        <a href={`${data.videoURL}`} target="_blank">View Video</a>
+        <div>
+            <a href={`/pdf/${data.pdf}`} target="_blank">
+                <p>View PDF</p>
+            </a>
+            <a href={data.videoURL} target="_blank">
+                <p>View Video</p>
+            </a>
+        </div>
+        <img src={`/img/${data.img}`} alt={data.title}/>
     {/if}
 </li>
 
 <style>
 
     li {
-        height: 20rem;
-        width: 20rem;
         background-color: var(--white);
-        border-radius: 2%;
         position: relative;
         transition: all .1s;
+        aspect-ratio: 8.5 / 11 ;
+        height: 25rem;
+        transition: all .2s;
+        cursor: pointer;
+        overflow: hidden;
+    }
+
+    li:hover div {
+        transform: translateX(0rem);
+    }
+
+    li:hover span {
+        transform: translateX(100%);
+    }
+
+    div {
+        background-color: var(--accent);
+        opacity: .95;
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        transform: translateX(-100%);
+        transition: all .2s;
 
         display: grid;
         grid-template-rows: repeat(2, 1fr);
     }
 
-    li:hover {
-        form: scale(1.01)
-    }
 
     a {
         place-self: center;
+        appearance: none;
+        text-decoration: none;
+        transition: all .2s;
     }
 
-    p {
+    a:hover {
+        transform: scale(1.05);
+    }
+
+    a p {
+        color: var(--white);
+        background-color: var(--black);
+        padding: 1rem;
+    }
+
+    span {
         background-color: var(--accent);
         color: var(--white);
         position: absolute;
         bottom: 0;
         right: 0;
         padding: .2rem;
+        transition: all .2s;
+    }
+
+    img {
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
     }
 
 </style>
